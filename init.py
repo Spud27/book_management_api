ecdfb70b3cfa3041134da9cb81b9dcb12ae07035
Michael <palumbo9673@gmail.com>
2025-03-25 14:58:13 +1100

    added env

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow 
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from flask_marshmallow import Marshmallow  # type: ignore
 
 db = SQLAlchemy()
 ma = Marshmallow()
